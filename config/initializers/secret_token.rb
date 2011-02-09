b0dd0ef92992a648213d2c6bb33dbd369b803734
Aaron Mills <aaron@jailbreakcreative.com>
2011-02-08 23:57:41 -0500

    Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LTP::Application.config.secret_token = '720aa0bcca6f4ff4f3e5a5f2650d33138f181f555588bba95a4aeea0203a898c255934be6e5f7c6b066e7e17b58ae57a6fd28db1880aef71bb7bc03711d5bb47'
